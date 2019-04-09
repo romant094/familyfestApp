6b3dfa27cc82dffe392f685b3c8a048015ee7320
Anton Romankov <romant094@gmail.com>
2019-04-10 00:26:33 +0300

    Questions could be switched now (1 forward and 1 backward)

--- js/script.js
+++ js/script.js
@@ -1,30 +1,48 @@
-let currentQuestion;
+let currentQuestion = 0;
 
 const questionsCount = 20,
     d = document,
     progressWrap = d.querySelector('.test__progress'),
     progressBar = d.querySelector('#progress'),
     testBlocks = d.querySelectorAll('.test__block'),
-    buttons = d.querySelectorAll('button'),
+    buttonStart = d.querySelector('button.start'),
+    buttonsPrev = d.querySelectorAll('button.prev'),
+    buttonsNext = d.querySelectorAll('button.next'),
     currentQuestionBlock = d.querySelector('#current-question');
+
+buttonsPrev[0].disabled = true;
+const lastIndex = buttonsNext.length-1;
+buttonsNext[lastIndex].disabled = true;
 
 const currentPercent = () => {
     progressBar.style.width = (currentQuestion / questionsCount) * 100 + '%';
 };
 
-buttons.forEach(function (btn, index) {
+buttonStart.addEventListener('click', function () {
+    changeQuestion(this);
+    progressWrap.classList.remove('disabled');
+});
+
+buttonsPrev.forEach(function (btn, index) {
     btn.addEventListener('click', function () {
-        currentQuestion = +this.dataset.question;
-
-        if (index === 0) {
-            progressWrap.classList.remove('disabled');
-        }
-
-        currentQuestionBlock.textContent = currentQuestion + 1;
-        currentPercent();
-
-        testBlocks[currentQuestion].classList.add('disabled');
-        testBlocks[currentQuestion + 1].classList.remove('disabled');
-
+        changeQuestion(this);
     })
 });
+
+buttonsNext.forEach(function (btn, index) {
+    btn.addEventListener('click', function () {
+        changeQuestion(this);
+    })
+});
+
+const changeQuestion = function (elem) {
+    let newQuestion = +elem.dataset.question,
+    diff = newQuestion - currentQuestion;
+
+    testBlocks[currentQuestion].classList.add('disabled');
+    testBlocks[currentQuestion + diff].classList.remove('disabled');
+    currentQuestion = newQuestion;
+
+    currentQuestionBlock.textContent = currentQuestion;
+    currentPercent();
+};
